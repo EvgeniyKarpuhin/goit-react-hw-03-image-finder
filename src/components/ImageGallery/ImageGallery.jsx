1f48ef704a300b560d6dac85bee5fa3fee7d22b9
EvgeniyKarpuhin <81818834+EvgeniyKarpuhin@users.noreply.github.com>
2021-11-18 02:33:08 +0200

    add

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -5,7 +5,7 @@ function ImageGallery({ state, onClick }) {
   return (
     <ul className="ImageGallery">
       {state.images.map((item) => {
-        return <ImageGalleryItem item={item} onClick={onClick} />;
+        return <ImageGalleryItem key={item.id} item={item} onClick={onClick} />;
       })}
     </ul>
   );
--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function ImageGalleryItem({ item, onClick }) {
   return (
-    <li id={item.id}>
+    <li>
       <img
         src={item.webformatURL}
         alt={item.id}
